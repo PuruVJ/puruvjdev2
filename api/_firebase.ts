6bcafd9ec2e95667b904d1bb4d0db1db8b88ee9d
Puru Vijay <awesomepuruvj@gmail.com>
2020-09-24 10:15:22 +0530

    replace fb secret key with environment variables

--- api/_firebase.ts
+++ api/_firebase.ts
@@ -1,9 +1,22 @@
 import admin from "firebase-admin";
 
-const serviceAccount = require("./fb-admin-key.json");
+const { FB_CLIENT_ID, FB_KEY, FB_CLIENT_EMAIL, FB_KEY_ID } = process.env;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    // @ts-ignore
+    type: "service_account",
+    project_id: "puruvjdev",
+    private_key_id: FB_KEY_ID,
+    private_key: FB_KEY,
+    client_email: FB_CLIENT_EMAIL,
+    client_id: FB_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-svz3l%40puruvjdev.iam.gserviceaccount.com",
+  }),
   databaseURL: "https://puruvjdev.firebaseio.com",
 });
 
