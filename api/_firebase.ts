7e4e42ee599748dd26cfa875d8ea3d125111496a
Puru Vijay <awesomepuruvj@gmail.com>
2020-09-24 10:21:32 +0530

    Change

--- api/_firebase.ts
+++ api/_firebase.ts
@@ -1,6 +1,12 @@
 import admin from "firebase-admin";
 
-const { FB_CLIENT_ID, FB_KEY, FB_CLIENT_EMAIL, FB_KEY_ID } = process.env;
+const {
+  FB_CLIENT_ID,
+  FB_KEY,
+  FB_CLIENT_EMAIL,
+  FB_KEY_ID,
+  FB_CLIENT_x509_CERT_URL,
+} = process.env;
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -14,8 +20,7 @@ admin.initializeApp({
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-svz3l%40puruvjdev.iam.gserviceaccount.com",
+    client_x509_cert_url: FB_CLIENT_x509_CERT_URL,
   }),
   databaseURL: "https://puruvjdev.firebaseio.com",
 });
