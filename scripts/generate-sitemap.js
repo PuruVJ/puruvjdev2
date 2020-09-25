10624670abf4893b0f4e9074e5cb9fc25006b994
Puru Vijay <awesomepuruvj@gmail.com>
2020-09-25 14:22:10 +0530

    Sitemap xml autogeneration

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,25 @@
+const { writeFile } = require("fs").promises;
+const glob = require("glob-promise");
+
+(async function () {
+  const files = await glob(
+    "../__sapper__/export/**/index.html",
+    "!../__sapper__/export/{art,blog,client,css,data,icons,media,photos}"
+  );
+
+  const urls = files
+    .map((file) =>
+      file.replace("../__sapper__/export/", "").replace("index.html", "")
+    )
+    .map((url) => `<url><loc>https://puruvj.dev/${url}</loc></url>`)
+    .join("\n");
+
+  const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>
+`;
+
+  // Write the file
+  await writeFile("../__sapper__/export/sitemap.xml", sitemapContent, "utf-8");
+})();
