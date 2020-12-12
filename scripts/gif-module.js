241b0052543a8b01bd895a619d85ae852e1881af
Puru Vijay <devpuruvj@gmail.com>
2020-12-12 19:27:43 +0530

    Revert

--- scripts/gif-module.js
+++ scripts/gif-module.js
@@ -7,7 +7,11 @@ require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
 
-// cloudinary.config(process.env.CLOUDINARY_URL);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
 /**
  * This function converts gif to video and returns the necessary markup
