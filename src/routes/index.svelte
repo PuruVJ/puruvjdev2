4dffcf4ab95342915a46f142432d4e77d811fc5b
Puru Vijay <devpuruvj@gmail.com>
2021-03-30 01:29:19 +0530

    Update description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -54,9 +54,9 @@
     <div id="written">
       <h1>Hi, I'm <mark>Puru</mark></h1>
       <h2 id="about-me">
-        I am a
+        I am a 19 y/o
         <mark>self-taught fullstack web developer</mark>
-        based in India. I make
+        based in India with 6 years of hobbyist experience. I make
         <mark>blazing fast and performant</mark>
         web apps. Like this blog.
       </h2>
@@ -71,7 +71,7 @@
   <br />
   <br />
   <section class="popular-blogs">
-    <h1>Blog</h1>
+    <h1>Popular posts</h1>
     <BlogList seeMore {blogsList} />
   </section>
   <br /><br />
